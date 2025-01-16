38f05be6bfc9b28a42234fa1656252425b4ef161
Sergey-Ado <adonisiy2@gmail.com>
2025-01-16 20:59:48 +0400

    feat: complite 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,25 @@
+const path = require('node:path');
+const fs = require('node:fs/promises');
+
+async function createBundle() {
+  const bundle = path.join(__dirname, 'project-dist', 'bundle.css');
+
+  await fs.access(bundle).then(
+    () => fs.rm(bundle),
+    () => {},
+  );
+
+  const stylesDir = path.join(__dirname, 'styles');
+  const objects = await fs.readdir(stylesDir, { withFileTypes: true });
+  const files = objects
+    .filter((object) => object.isFile())
+    .map((file) => file.name)
+    .filter((filename) => path.parse(filename).ext === '.css');
+
+  for (let file of files) {
+    const data = await fs.readFile(path.join(stylesDir, file));
+    await fs.appendFile(bundle, data);
+  }
+}
+
+createBundle();
